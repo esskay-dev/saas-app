2efb23714d0ab222f4f3a85f187430893f82eba2
Salako <salakonuel@gmail.com>
2026-09-21 19:24:56 +0100

    Style navbar with sticky position and backdrop blur

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -19,11 +19,11 @@ export const Navbar = () => {
             <Image src={Logo} alt="Logo" height={40} width={40} />
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
-              <a href="">About</a>
-              <a href="">Features</a>
-              <a href="">Customers</a>
-              <a href="">Updates</a>
-              <a href="">Help</a>
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">Help</a>
               <button className="bg-black rounded-lg text-white px-4 py-2 font-medium inline-flex align-items justify-center tracking-tight">
                 Get for free
               </button>
