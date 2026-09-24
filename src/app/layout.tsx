6a7a9ff7be4c0e8993a7546b5ac11f654e39db9c
Salako <salakonuel@gmail.com>
2026-09-24 10:49:45 +0100

    feat: change title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "Pathway",
+  description: "Streamline your workflow and boost your productivity.",
 };
 
 export default function RootLayout({
